158f0b916cb97c8b762ad24906fa6aba65c5bfbd
Artur Foden <adfoden@gmail.com>
2024-09-28 01:24:52 +0100

    Updated url in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description="Type hints for Python astronomy projects",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/my_project", 
+    url="https://github.com/Arturius771/astronomy_types", 
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
