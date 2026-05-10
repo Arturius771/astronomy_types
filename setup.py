7dd07ad242b77708f130ffb66d5a19d6e58c14a4
Artur Foden <adfoden@gmail.com>
2026-05-10 01:29:27 +0100

    Release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="astronomy_types",
-    version="2.0.0",
+    version="2.1.0",
     py_modules=["astronomy_types"],  # without .py
     author="Artur Foden",
     author_email="",
